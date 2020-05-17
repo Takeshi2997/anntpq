f95e207f1e9ab7f4e47596182f4652013f8f7bac
Takeshi2997 <takeshi6.6260@gmail.com>
2020-05-18 06:00:33 +0900

    roop num update

--- setup.jl
+++ setup.jl
@@ -9,9 +9,9 @@ const t = 1.0f0
 const J = 1.0f0
 
 # Repeat Number
-const burnintime = 100
-const iters_num = 200
-const it_num = 1000
+const burnintime = 20
+const iters_num = 100
+const it_num = 500
 const iϵmax = 10
 const num = 10000
 
@@ -20,6 +20,6 @@ const layer = [dimB+dimS, 88, 88, 88, 2]
 const layers_num = length(layer) - 1
 
 # Learning Rate
-const lr = 0.001f0
+const lr = 0.01f0
 
 end
