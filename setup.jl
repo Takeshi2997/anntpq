1f46c5c964a83888e7add1759ce352f44f65d457
Takeshi2997 <takeshi6.6260@gmail.com>
2020-05-18 14:24:26 +0900

    1000 roop lr fix

--- setup.jl
+++ setup.jl
@@ -11,7 +11,7 @@ const J = 1.0f0
 # Repeat Number
 const burnintime = 100
 const iters_num = 500
-const it_num = 2000
+const it_num = 1000
 const iϵmax = 10
 const num = 10000
 
@@ -20,6 +20,6 @@ const layer = [dimB+dimS, 88, 88, 2]
 const layers_num = length(layer) - 1
 
 # Learning Rate
-const lr = 0.0001f0
+const lr = 0.001f0
 
 end
