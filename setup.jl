20103e6152fa88430e08434f732d8aea1f366095
Takeshi2997 <takeshi6.6260@gmail.com>
2020-05-21 11:49:42 +0900

    pre honban

--- setup.jl
+++ setup.jl
@@ -7,7 +7,7 @@ const dimB = 40
 # System Param
 const t = 1.0f0
 const J = 1.0f0
-const λ = 0.01f0
+const λ = 0.1f0
 
 # Repeat Number
 const burnintime = 100
@@ -21,6 +21,6 @@ const layer = [dimB+dimS, 24, 24, 2]
 const layers_num = length(layer) - 1
 
 # Learning Rate
-const lr = 0.001f0
+const lr = 0.0001f0
 
 end
