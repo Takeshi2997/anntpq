1b337efef83bff70ea9046d52fef3eeb083f57d2
Takeshi2997 <takeshi6.6260@gmail.com>
2020-05-26 05:41:43 +0900

    lambda change

--- main.jl
+++ main.jl
@@ -54,8 +54,8 @@ function main()
         energyS = 0.0f0
         energyB = 0.0f0
         numberB = 0.0f0
-        lr      = ifelse(iϵ > 1, Const.lr, 0.001f0)
-        it_num  = ifelse(iϵ > 1, Const.it_num, 5000)
+        lr      = Const.lr
+        it_num  = Const.it_num
 
         # Learning
         filename = dirnameerror * "/error" * lpad(iϵ, 3, "0") * ".txt"
--- setup.jl
+++ setup.jl
@@ -7,7 +7,7 @@ const dimB = 80
 # System Param
 const t = 1.0f0
 const J = 1.0f0
-const λ = 0.01f0
+const λ = 0.1f0
 const η = 0.5f0
 
 # Repeat Number
@@ -22,6 +22,6 @@ const layer = [dimB+dimS, 48, 48, 2]
 const layers_num = length(layer) - 1
 
 # Learning Rate
-const lr = 0.0001f0
+const lr = 0.001f0
 
 end
