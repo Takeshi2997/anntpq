180789eb75d637db4f7eed9e3061eb63cd2a4e85
Takeshi2997 <takeshi6.6260@gmail.com>
2020-06-12 05:11:43 +0900

    fix

--- main.jl
+++ main.jl
@@ -57,7 +57,7 @@ function main()
     energyB = 0f0
     numberB = 0f0
     lr      = 0.001f0
-    it_num  = Const.it_num
+    it_num  = 200
 
     # Learning
     filename = dirnameerror * "/error000.txt"
--- setup.jl
+++ setup.jl
@@ -13,7 +13,7 @@ const η = 0.5f0
 # Repeat Number
 const burnintime = 10
 const iters_num = 500
-const it_num = 200
+const it_num = 1000
 const iϵmax = 10
 const num = 10000
 
