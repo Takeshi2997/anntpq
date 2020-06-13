81752928541cc63c743de7637d6151730cb2ac87
Takeshi2997 <takeshi6.6260@gmail.com>
2020-06-14 04:59:14 +0900

    lr change

--- setup.jl
+++ setup.jl
@@ -7,13 +7,12 @@ const dimB = 120
 # System Param
 const t = 1.0f0
 const J = 1.0f0
-const λ = 0.001f0
 const η = 0.5f0
 
 # Repeat Number
 const burnintime = 10
-const iters_num = 500
-const it_num = 1000
+const iters_num = 1000
+const it_num = 200
 const iϵmax = 10
 const num = 10000
 
@@ -22,6 +21,6 @@ const layer = [dimB+dimS, 56, 56, 56, 2]
 const layers_num = length(layer) - 1
 
 # Learning Rate
-const lr = 0.0001f0
+const lr = 0.001f0
 
 end
