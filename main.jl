c04376717c52842f2f86aa510cf9a6a1ae2392d7
Takeshi2997 <takeshi6.6260@gmail.com>
2020-07-16 05:37:54 +0900

    fix

--- main.jl
+++ main.jl
@@ -47,7 +47,7 @@ function main()
     g = open("error.txt", "w")
 
     # iϵ = 0
-    ϵ = 0.1f0 * Const.t * Const.dimB
+    ϵ = 0.0f0 * Const.t * Const.dimB
     filenameparams = dirname * "/params_at_000.bson"
 
     # Initialize
@@ -56,7 +56,7 @@ function main()
     energyS = 0f0
     energyB = 0f0
     numberB = 0f0
-    lr      = 0.0001f0
+    lr      = -0.0001f0
     it_num  = 1000
 
     # Learning
