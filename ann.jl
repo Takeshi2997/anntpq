9988f6bd8d593d98e98c06ca1ed1a8df54f21761
Takeshi2997 <takeshi6.6260@gmail.com>
2020-12-14 06:00:16 +0900

    first commit

--- ann.jl
+++ ann.jl
@@ -18,30 +18,33 @@ oe = Vector{Parameters}(undef, Const.layers_num)
 
 function initO()
 
-    for i in 1:Const.layers_num
+    for i in 1:Const.layers_num-1
         W = zeros(Complex{Float32}, Const.layer[i+1], Const.layer[i])
         b = zeros(Complex{Float32}, Const.layer[i+1])
         global o[i]  = Parameters(W, b)
         global oe[i] = Parameters(W, b)
     end
+    W = zeros(Complex{Float32}, Const.layer[end], Const.layer[end-1])
+    b = zeros(Complex{Float32}, Const.layer[1])
+    global o[end]  = Parameters(W, b)
+    global oe[end] = Parameters(W, b)
 end
 
-struct Res{F,S<:AbstractArray,T<:AbstractArray}
+struct Output{S<:AbstractArray,T<:AbstractArray}
     W::S
     b::T
-    σ::F
 end
 
-function Res(in::Integer, out::Integer, σ = identity;
+function Output(in::Integer, out::Integer, out2::Integer;
              initW = Flux.glorot_uniform, initb = zeros)
-  return Res(initW(out, in), initb(Float32, out), σ)
+  return Output(initW(out, in), initb(Float32, out2))
 end
 
-@functor Res
+@functor Output
 
-function (m::Res)(x::AbstractArray)
-    W, b, σ = m.W, m.b, m.σ
-    x .+ σ.(W*x.+b)
+function (m::Output)(x::AbstractArray)
+    W, b = m.W, m.b
+    W*x, b
 end
 
 mutable struct Network
@@ -54,9 +57,9 @@ function Network()
 
     layer = Vector{Any}(undef, Const.layers_num)
     for i in 1:Const.layers_num-1
-        layer[i] = Res(Const.layer[i], Const.layer[i+1], tanh)
+        layer[i] = Dense(Const.layer[i], Const.layer[i+1], tanh)
     end
-    layer[end] = Dense(Const.layer[end-1], Const.layer[end])
+    layer[end] = Output(Const.layer[end-1], Const.layer[end], Const.layer[1])
     f = Chain([layer[i] for i in 1:Const.layers_num]...)
     p = Flux.params(f)
     Network(f, p)
@@ -80,11 +83,17 @@ end
 function init()
 
     parameters = Vector{Array}(undef, Const.layers_num)
-    for i in 1:Const.layers_num
+    for i in 1:Const.layers_num-1
         W = Flux.glorot_uniform(Const.layer[i+1], Const.layer[i])
         b = zeros(Float32, Const.layer[i+1])
         parameters[i] = [W, b]
     end
+    e = Exponential(2f0)
+    W = Array{Float32, 2}(undef, Const.layer[end], Const.layer[end-1])
+    W[1, :] = log.(sqrt.(rand(e, Const.layer[end-1])))
+    W[2, :] = rand(Float32, Const.layer[end-1]) .* π
+    b = zeros(Float32, Const.layer[1])
+    parameters[end] = [W, b]
  
     paramset = [param for param in parameters]
     p = Flux.params(paramset...)
@@ -93,8 +102,8 @@ end
 
 function forward(x::Vector{Float32})
 
-    out = network.f(x)
-    return out[1] + im * out[2]
+    out, b = network.f(x)
+    return out[1] + im * out[2] + transpose(b) * x
 end
 
 loss(x::Vector{Float32}) = real(forward(x))
@@ -102,7 +111,7 @@ loss(x::Vector{Float32}) = real(forward(x))
 function backward(x::Vector{Float32}, e::Complex{Float32})
 
     gs = gradient(() -> loss(x), network.p)
-    for i in 1:Const.layers_num-1
+    for i in 1:Const.layers_num
         dw = gs[network.f[i].W]
         db = gs[network.f[i].b]
         o[i].W  += dw
@@ -110,9 +119,6 @@ function backward(x::Vector{Float32}, e::Complex{Float32})
         o[i].b  += db
         oe[i].b += db * e
     end
-    dw = gs[network.f[end].W]
-    o[end].W  += dw
-    oe[end].W += dw * e
 end
 
 opt(lr::Float32) = ADAM(lr, (0.9, 0.999))
@@ -121,14 +127,12 @@ function update(energy::Float32, ϵ::Float32, lr::Float32)
 
     x = (energy - ϵ)
     α = 2f0 * x / Const.iters_num
-    for i in 1:Const.layers_num-1
+    for i in 1:Const.layers_num
         ΔW = α .* 2f0 .* real.(oe[i].W .- energy * o[i].W)
         Δb = α .* 2f0 .* real.(oe[i].b .- energy * o[i].b)
         update!(opt(lr), network.f[i].W, ΔW)
         update!(opt(lr), network.f[i].b, Δb)
     end
-    ΔW = α .* 2f0 * real(oe[end].W .- energy * o[end].W)
-    update!(opt(lr), network.f[end].W, ΔW)
 end
 
 end
