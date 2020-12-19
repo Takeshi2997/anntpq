36f81e60aa0bc87cd44521f3503520056c383ab5
Takeshi2997 <takeshi6.6260@gmail.com>
2020-12-19 11:05:39 +0900

    qrms ver

--- ann.jl
+++ ann.jl
@@ -2,7 +2,6 @@ module ANN
 include("./setup.jl")
 using .Const, LinearAlgebra, Flux, Zygote
 using Flux: @functor
-using Flux.Optimise: update!
 using BSON: @save
 using BSON: @load
 
@@ -84,11 +83,11 @@ end
 function init()
     parameters = Vector{Array}(undef, Const.layers_num)
     for i in 1:Const.layers_num-1
-        W = Flux.glorot_uniform(Const.layer[i+1], Const.layer[i])
+        W = Flux.glorot_uniform(Const.layer[i+1], Const.layer[i]) .* 2.25f0
         b = zeros(Float32, Const.layer[i+1])
         parameters[i] = [W, b]
     end
-    W = Flux.glorot_uniform(Const.layer[end], Const.layer[end-1])
+    W = Flux.glorot_uniform(Const.layer[end], Const.layer[end-1]) .* 2.25f0
     b = zeros(Float32, Const.layer[1])
     parameters[end] = [W, b]
     paramset = [param for param in parameters]
@@ -117,7 +116,7 @@ function backward(x::Vector{Float32}, e::Complex{Float32})
     end
 end
 
-opt(lr::Float32) = ADAM(lr, (0.9, 0.999))
+opt(lr::Float32) = QRMSProp(lr, 0.9)
 
 function update(energy::Float32, ϵ::Float32, lr::Float32)
     x = 2f0 * (energy - ϵ)
@@ -125,9 +124,37 @@ function update(energy::Float32, ϵ::Float32, lr::Float32)
     for i in 1:Const.layers_num
         ΔW = α .* 2f0 .* real.(oe[i].W .- energy * o[i].W)
         Δb = α .* 2f0 .* real.(oe[i].b .- energy * o[i].b)
-        update!(opt(lr), network.f[i].W, ΔW)
-        update!(opt(lr), network.f[i].b, Δb)
+        update!(opt(lr), network.f[i].W, ΔW, o[i].W)
+        update!(opt(lr), network.f[i].b, Δb, o[i].b)
     end
+end
+
+const ϵ = 1f-8
+
+mutable struct QRMSProp
+  eta::Float32
+  rho::Float32
+  acc::IdDict
+end
+
+QRMSProp(η = 0.001f0, ρ = 0.9f0) = QRMSProp(η, ρ, IdDict())
+
+function apply!(o::QRMSProp, x, g, O)
+  η, ρ = o.eta, o.rho
+  acc = get!(o.acc, x, zero(x))::typeof(x)
+  @. acc = ρ * acc + (1 - ρ) * abs2(O)
+  @. g *= η / (√acc + ϵ)
+end
+
+function update!(opt, x, x̄, x̂)
+  x .-= apply!(opt, x, x̄, x̂)
+end
+
+function update!(opt, xs::Params, gs, o)
+  for x in xs
+    gs[x] == nothing && continue
+    update!(opt, x, gs[x], o)
+  end
 end
 
 end
