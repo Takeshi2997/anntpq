2c7fd0e2ea25b257309e586d22a5381115f64e9a
Takeshi2997 <takeshi6.6260@gmail.com>
2020-12-24 08:29:42 +0900

    qrmsprop

--- ann.jl
+++ ann.jl
@@ -2,7 +2,6 @@ module ANN
 include("./setup.jl")
 using .Const, LinearAlgebra, Flux, Zygote, Distributions
 using Flux: @functor
-using Flux.Optimise: update!
 using BSON: @save
 using BSON: @load
 
