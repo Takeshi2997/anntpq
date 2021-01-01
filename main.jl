e49ca7aeb09c914691b5608c177c7527ab5d3e51
Takeshi2997 <takeshi6.6260@gmail.com>
2021-01-01 19:13:48 +0900

    fixed

--- main.jl
+++ main.jl
@@ -49,7 +49,7 @@ function main()
     mkdir(dirnameerror)
     MLcore.Func.ANN.init()
     MLcore.Func.ANN.save("./data/params_at_000.bson")
-    learning(0, dirname, dirnameerror, 0.0005f0, 10000)
+    learning(0, dirname, dirnameerror, 0.0001f0, 50000)
 
     @time pmap(iϵ -> learning(iϵ, dirname, dirnameerror, Const.lr, Const.it_num), 1:Const.iϵmax)
 end
--- setup.jl
+++ setup.jl
@@ -10,9 +10,9 @@ const J = 1.0f0
 
 # Repeat Number
 const burnintime = 100
-const iters_num = 500
-const it_num = 10000
-const iϵmax = 12
+const iters_num = 1000
+const it_num = 5000
+const iϵmax = 8
 const num = 10000
 
 # Network Params
@@ -20,6 +20,6 @@ const layer = [dimB+dimS, 48, 48, 48, 48, 2]
 const layers_num = length(layer) - 1
 
 # Learning Rate
-const lr = 0.0001f0
+const lr = 0.001f0
 
 end
