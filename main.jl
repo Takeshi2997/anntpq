77c17704d62d0bc3b9dc183e019ff024bfab77a8
Takeshi2997 <takeshi6.6260@gmail.com>
2021-01-01 10:09:01 +0900

    fixed

--- main.jl
+++ main.jl
@@ -49,7 +49,7 @@ function main()
     mkdir(dirnameerror)
     MLcore.Func.ANN.init()
     MLcore.Func.ANN.save("./data/params_at_000.bson")
-    learning(0, dirname, dirnameerror, Const.lr, 20000)
+    learning(0, dirname, dirnameerror, 0.0005f0, 10000)
 
     @time pmap(iϵ -> learning(iϵ, dirname, dirnameerror, Const.lr, Const.it_num), 1:Const.iϵmax)
 end
