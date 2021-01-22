53994c59e41d3be1a33ebc6edd275b8dc5d872f9
Takeshi2997 <takeshi6.6260@gmail.com>
2021-01-23 07:08:09 +0900

    fixed

--- setup.jl
+++ setup.jl
@@ -1,22 +1,22 @@
 module Const
 
 # System Size
-const dimS = 16
-const dimB = 80
+const dimS = 2
+const dimB = 72
 
 # System Param
 const t = 1.0f0
 const J = 1.0f0
 
 # Repeat Number
-const burnintime = 100
+const burnintime = 10
 const iters_num = 200
-const it_num = 10000
+const it_num = 5000
 const iϵmax = 4
 const num = 100000
 
 # Network Params
-const layer = [dimB+dimS, 48, 48, 48, 48, 2]
+const layer = [dimB+dimS, 80, 80, 80, 80, 2]
 const layers_num = length(layer) - 1
 
 # Learning Rate
