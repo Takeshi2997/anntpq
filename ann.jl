1f2862b04158028bc76cf00c334c8b4d686617cc
Takeshi2997 <takeshi6.6260@gmail.com>
2021-01-24 14:59:46 +0900

    fixed

--- ann.jl
+++ ann.jl
@@ -42,6 +42,20 @@ function (m::Res)(x::AbstractArray)
     x .+ σ.(W*x.+b)
 end
 
+struct Output{S<:AbstractArray,T<:AbstractArray}
+    W::S
+    b::T
+end
+function Output(in::Integer, out::Integer, first::Integer;
+                initW = Flux.glorot_uniform, initb = Flux.zeros)
+  return Output(initW(out, in), initb(first))
+end
+@functor Output
+function (m::Output)(x::AbstractArray)
+    W, b = m.W, m.b
+    W*x, b
+end
+
 mutable struct Network
     f::Flux.Chain
     p::Zygote.Params
@@ -52,7 +66,7 @@ function Network()
     for i in 1:Const.layers_num-1
         layer[i] = Res(Const.layer[i], Const.layer[i+1], tanh)
     end
-    layer[end] = Dense(Const.layer[end-1], Const.layer[end])
+    layer[end] = Output(Const.layer[end-1], Const.layer[end], Const.layer[1])
     f = Chain([layer[i] for i in 1:Const.layers_num]...)
     p = Flux.params(f)
     Network(f, p)
@@ -75,11 +89,14 @@ end
 
 function init()
     parameters = Vector{Array}(undef, Const.layers_num)
-    for i in 1:Const.layers_num
-        W = Flux.glorot_uniform(Const.layer[i+1], Const.layer[i]) 
+    for i in 1:Const.layers_num-1
+        W = randn(Float32, Const.layer[i+1], Const.layer[i]) ./ Const.layer[i]
         b = Flux.zeros(Const.layer[i+1])
         parameters[i] = [W, b]
     end
+    W = randn(Float32, Const.layer[end], Const.layer[end-1]) ./ Const.layer[end-1]
+    b = rand(Float32, Const.layer[1]) .* π
+    parameters[end] = [W, b]
     paramset = [param for param in parameters]
     p = Flux.params(paramset...)
     Flux.loadparams!(network.f, p)
@@ -88,15 +105,16 @@ end
 # Learning Method
 
 function forward(x::Vector{Float32})
-    out = network.f(x)
-    return out[1] + im * out[2]
+    Out, b = network.f(x)
+    B = transpose(x) * b
+    return Out[1] + im * Out[2] + im * B
 end
 
 loss(x::Vector{Float32}) = real(forward(x))
 
 function backward(x::Vector{Float32}, e::Complex{Float32})
     gs = gradient(() -> loss(x), network.p)
-    for i in 1:Const.layers_num-1
+    for i in 1:Const.layers_num
         dw = gs[network.f[i].W]
         db = gs[network.f[i].b]
         o[i].W  += dw
@@ -104,23 +122,18 @@ function backward(x::Vector{Float32}, e::Complex{Float32})
         o[i].b  += db
         oe[i].b += db .* e
     end
-    dw = gs[network.f[end].W]
-    o[end].W  += dw
-    oe[end].W += dw .* e
 end
 
 opt(lr::Float32) = ADAM(lr, (0.9, 0.999))
 
 function update(energy::Float32, ϵ::Float32, lr::Float32)
     α = 2f0 .* (energy - ϵ) / Const.iters_num
-    for i in 1:Const.layers_num-1
+    for i in 1:Const.layers_num
         ΔW = α .*  real.(oe[i].W .- energy * o[i].W)
         Δb = α .*  real.(oe[i].b .- energy * o[i].b)
         update!(opt(lr), network.f[i].W, ΔW)
         update!(opt(lr), network.f[i].b, Δb)
     end
-    ΔW = α .* real.(oe[end].W .- energy * o[end].W)
-    update!(opt(lr), network.f[end].W, ΔW)
 end
 
 end
