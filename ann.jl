5c086ec80a5f63e094d05587b2a1f1b1e3973db8
Takeshi2997 <takeshi6.6260@gmail.com>
2021-02-06 09:12:17 +0900

    fixed

--- ann.jl
+++ ann.jl
@@ -1,6 +1,6 @@
 module ANN
 include("./setup.jl")
-using .Const, LinearAlgebra, Flux, Zygote, CUDA
+using .Const, LinearAlgebra, Flux, Zygote, CUDA, BlockDiagonal
 using Flux: @functor
 using Flux.Optimise: update!
 using BSON: @save
@@ -8,24 +8,18 @@ using BSON: @load
 
 # Initialize Variables
 
-o   = Vector{Array}(undef, Const.layers_num)
-oe  = Vector{Array}(undef, Const.layers_num)
-oo  = Array{Array}(undef, Const.layers_num, Const.layers_num)
-const I   = [Diagonal(CUDA.ones(Float32, Const.layer[i+1] * (Const.layer[i] + 1)))
-             for i in 1:Const.layers_num]
+o   = Array{Array}(undef, Const.layers_num)
+oe  = Array{Array}(undef, Const.layers_num)
+oo  = Array{BlockDiagonal}(undef, Const.layers_num)
+const I = [Diagonal(CUDA.ones(Float32, Const.layer[i+1] * (Const.layer[i] + 1))) for i in 1:Const.layers_num]
 
 function initO()
     for i in 1:Const.layers_num
-        j = i%Const.layers_num + 1
-        k = j%Const.layers_num + 1
-        W  = zeros(Complex{Float32}, Const.layer[j] * (Const.layer[i] + 1))
-        W′ = zeros(Complex{Float32}, Const.layer[k] * (Const.layer[j] + 1))
+        W  = zeros(Complex{Float32}, Const.layer[i+1] * (Const.layer[i] + 1))
         S  = transpose(W) .* W
-        S′ = transpose(W′) .* W
-        global o[i]   = W
-        global oe[i]  = W
-        global oo[i, i] = S
-        global oo[j, i] = S′
+        global o[i]  = W
+        global oe[i] = W
+        global oo[i] = S
     end
 end
 
@@ -46,22 +40,18 @@ function (m::Layer)(x::AbstractArray)
     σ.(W*z)
 end
 
-struct Output{F,S<:AbstractArray}
+struct Output{S<:AbstractArray}
     W::S
-    σ::F
 end
-function Output(in::Integer, out::Integer, σ = identity;
-                initW = randn)
-    return Output(initW(Complex{Float32}, out, in+1), σ)
+function Output(in::Integer, out::Integer;
+                initW = Flux.glorot_uniform)
+    return Output(initW(out, in))
 end
 @functor Output
 function (m::Output)(x::AbstractArray)
-    W, σ = m.W, m.σ
-    z = vcat(x, 1)
-    σ.(W*z)
+    W = m.W
+    W*x
 end
-
-NNlib.logcosh(z::Complex) = log(cosh(z))
 
 mutable struct Network
     f::Flux.Chain
@@ -73,7 +63,7 @@ function Network()
     for i in 1:Const.layers_num-1
         layers[i] = Layer(Const.layer[i], Const.layer[i+1], tanh)
     end
-    layers[end] = Output(Const.layer[end-1], Const.layer[end], logcosh)
+    layers[end] = Output(Const.layer[end-1], Const.layer[end])
     f = Chain([layers[i] for i in 1:Const.layers_num]...)
     p = Flux.params(f)
     Network(f, p)
@@ -100,7 +90,7 @@ function init()
         W = Flux.glorot_uniform(Const.layer[i+1], Const.layer[i]+1) 
         parameters[i] = [W]
     end
-    W = randn(Complex{Float32}, Const.layer[end], Const.layer[end-1]+1) .* sqrt(2f0 / (Const.layer[end-1]+1))
+    W = Flux.glorot_uniform(Const.layer[end], Const.layer[end-1]) 
     parameters[end] = [W]
     paramset = [param for param in parameters]
     p = Flux.params(paramset...)
@@ -111,42 +101,35 @@ end
 
 function forward(x::Vector{Float32})
     out = network.f(x)
-    return sum(out)
+    return out[1] + im * out[2]
 end
 
 loss(x::Vector{Float32}) = real(forward(x))
 
 function backward(x::Vector{Float32}, e::Complex{Float32})
     gs = gradient(() -> loss(x), network.p)
-    dw = [reshape(gs[network.f[i].W], length(gs[network.f[i].W])) for i in 1:Const.layers_num]
     for i in 1:Const.layers_num
-        j = i%Const.layers_num + 1
-        o[i]  += dw[i]
-        oe[i] += dw[i] .* e
-        oo[i, i] += transpose(dw[i]) .* conj.(dw[i])
-        oo[j, i] += transpose(dw[j]) .* conj.(dw[i])
+        dw = gs[network.f[i].W]
+        oo[i] = BlockDiagonal([transpose(dw[:, j]) .* conj.(dw[:, j]) for j in 1:axes(dw, 2)])
+        dw = reshape(dw, length(dw))
+        o[i]  += dw
+        oe[i] += dw .* e
     end
 end
 
 opt(lr::Float32) = Descent(lr)
 
 function update(energy::Float32, ϵ::Float32, lr::Float32)
-    α = 1f0 / Const.iters_num
     for i in 1:Const.layers_num
-        j = i%Const.layers_num + 1
         o[i]  ./= Const.iters_num
         oe[i] ./= Const.iters_num 
-        oo[i, i] ./= Const.iters_num 
-        oo[j, i] ./= Const.iters_num 
+        oo[i] ./= Const.iters_num 
     end
     for i in 1:Const.layers_num
-        j = i%Const.layers_num + 1
-        R  = CuArray(2f0 .* real.(o[i] .- (ϵ - energy) * o[i]))
-        R′ = CuArray(2f0 .* real.(o[j] .- (ϵ - energy) * o[j]))
-        S  = CuArray(2f0 .* energy .* real.(oo[i, i] - transpose(o[i]) .* conj.(o[i])))
-        S′ = CuArray(2f0 .* energy .* real.(oo[j, i] - transpose(o[j]) .* conj.(o[i])))
-        ΔW = reshape((S′ .+ Const.η .* I[j])\R′ - (S .+ Const.η .* I[i])\R, 
-                     (Const.layer[i+1], Const.layer[i]+1)) |> cpu
+        R   = CuArray(2f0 * real.(o[i] - (ϵ - energy) * o[i]))
+        OO  = BlockDiagonal([transpose(o[:, j]) .* conj.(o[:, j]) for j in 1:axes(o, 2)])
+        S   = CuArray(2f0 * real.(oo[i] - OO))
+        ΔW  = reshape((S + Const.η * I[i])\R, (Const.layer[i+1], Const.layer[i]+1)) |> cpu
         update!(opt(lr), network.f[i].W, ΔW)
     end
 end
--- setup.jl
+++ setup.jl
@@ -10,16 +10,16 @@ const J = 1.0f0
 
 # Repeat Number
 const burnintime = 10
-const iters_num = 500
+const iters_num = 1000
 const it_num = 1000
 const iϵmax = 1
-const η = 0.001f0
 
 # Network Params
-const layer = [dimB+dimS, 40]
+const layer = [dimB+dimS, 20, 2]
 const layers_num = length(layer) - 1
+const η = 0.001f0
 
 # Learning Rate
-const lr = 1f-2
+const lr = 1f-4
 
 end
