d36b96cced7c7b3a5b8e48ac637757e8aacb32f0
Takeshi2997 <takeshi6.6260@gmail.com>
2021-02-10 06:56:35 +0900

    fixed

--- setup.jl
+++ setup.jl
@@ -10,16 +10,16 @@ const J = 1.0f0
 
 # Repeat Number
 const burnintime = 10
-const iters_num = 200
+const iters_num = 500
 const it_num = 1000
 const iϵmax = 1
 const inv_n = 20
 
 # Network Params
-const layer = [dimB+dimS, 40, 40, 2]
+const layer = [dimB+dimS, 40, 40, 40, 2]
 const layers_num = length(layer) - 1
 
 # Learning Rate
-const lr = 1f-4
+const lr = 1f-6
 
 end
