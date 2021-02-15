25f82f8a7d74101e96aa903d1a765c034b5a3df9
Takeshi2997 <takeshi6.6260@gmail.com>
2021-02-16 01:45:06 +0900

    fixed

--- setup.jl
+++ setup.jl
@@ -11,13 +11,13 @@ const J = 1.0f0
 # Repeat Number
 const burnintime = 10
 const iters_num = 500
-const it_num = 25
+const it_num = 50
 const inv_n = 20
 const iϵmax = 4
 
 # Network Params
 const layer = [dimB+dimS, 48, 48, 48, 2]
 const layers_num = length(layer) - 1
-const batchsize = 80
+const batchsize = 160
 
 end
