1a0c1082c12c87f1753a9b8be60aab1785a9d1f0
Takeshi2997 <takeshi6.6260@gmail.com>
2021-02-18 19:15:54 +0900

    fixed

--- ann.jl
+++ ann.jl
@@ -159,9 +159,11 @@ function updateparams(e::Float32, lr::Float32, paramset::ParamSet, Δparamset::V
     X = 1f0 / sqrt(real(paramset.x.ϕ))
     for i in 1:Const.layers_num
         Δparamset[i][1] += 
-        real.(paramset.oe[i].W - e * paramset.o[i].W) - X * (real.(paramset.ob[i].W) - real.(paramset.o[i].W) .* real.(paramset.b.ϕ))
+        real.(paramset.oe[i].W - e * paramset.o[i].W) - 
+        X * (real.(paramset.ob[i].W) - 2f0 .* real.(paramset.o[i].W) .* real.(paramset.b.ϕ))
         Δparamset[i][2] += 
-        real.(paramset.oe[i].b - e * paramset.o[i].b) - X * (real.(paramset.ob[i].b) - real.(paramset.o[i].b) .* real.(paramset.b.ϕ))
+        real.(paramset.oe[i].b - e * paramset.o[i].b) - 
+        X * (real.(paramset.ob[i].b) - 2f0 .* real.(paramset.o[i].b) .* real.(paramset.b.ϕ))
     end
 end
 
