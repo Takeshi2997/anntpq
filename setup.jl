5ceee9ae280dd02cbca5dab126f2ba790ebf1b2f
Takeshi2997 <takeshi6.6260@gmail.com>
2021-03-16 18:49:39 +0900

    fix

--- setup.jl
+++ setup.jl
@@ -18,10 +18,11 @@ const iϵmax = 4
 const lr = 1f-3
 
 # Network Params
-const layer1 = [dimB,     40, 40, 40, 2]
-const layer2 = [dimS,     16, 16, 16, 2]
-const layer3 = [2 * dimS, 16, 16, 16, 2]
+const layer1 = [dimB,     80, 2]
+const layer2 = [dimS,     16, 2]
+const layer3 = [2 * dimS, 32, 2]
 const layers_num = length(layer3) - 1
 const batchsize = 128
+const λ = 0.001f0
 
 end
