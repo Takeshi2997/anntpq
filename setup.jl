f60de00e5c41cf5aa9e74009bbc69d5f8032dd64
Takeshi2997 <takeshi6.6260@gmail.com>
2021-03-26 09:46:50 +0900

    fix

--- setup.jl
+++ setup.jl
@@ -7,20 +7,20 @@ const dimB = 80
 # System Param
 const t = 1.0f0
 const J = 1.0f0
-const λ = 1f-4
+const λ = 1f-2
 
 # Repeat Number
 const burnintime = 10
-const iters_num = 100
+const iters_num = 1000
 const it_num = 100
 const iϵmax = 4
 
 # Learning Rate
-const lr = 1f-3
+const lr = 1f-5
 
 # Network Params
 const layer1 = [dimB,      40, 40, 2]
-const layer2 = [dimS,       8,  8, 2]
+const layer2 = [dimS,      16, 16, 2]
 const layer3 = [dimB+dimS, 48, 48, 2]
 const layers_num = length(layer3) - 1
 const batchsize = 64
