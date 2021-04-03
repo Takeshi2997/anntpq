a4cc2c6a154ceac13325c338a894a04fbc278ff8
Takeshi2997 <takeshi6.6260@gmail.com>
2021-04-03 15:45:12 +0900

    fix

--- ann.jl
+++ ann.jl
@@ -129,7 +129,8 @@ function forward(x::Vector{Float32})
     return out1[1] + im * out1[2] + out2[1] + im * out2[2] + out3[1] + im * out3[2]
 end
 
-loss(x::Vector{Float32}) = real(forward(x))
+sqnorm(x) = sum(abs2, x)
+loss(x::Vector{Float32}) = real(forward(x)) + sum(sqnorm, Flux.params(network.f[3]))
 
 function backward(x::Vector{Float32}, e::Complex{Float32}, paramset::ParamSet)
     gs1 = gradient(() -> loss(x), network.p[1])
