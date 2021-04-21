96804ca50562158908a6e76abce6ba3d9ce1bbc0
Takeshi2997 <takeshi6.6260@gmail.com>
2021-04-22 08:00:22 +0900

    fix

--- setup.jl
+++ setup.jl
@@ -2,7 +2,7 @@ module Const
 
 # System Size
 const dimS = 8
-const dimB = 16
+const dimB = 56
 
 # System Param
 const t = 1f0
@@ -16,13 +16,13 @@ const it_num = 100
 const iϵmax = 1
 
 # Network Params
-const layer = [dimB+dimS, 24]
+const layer = [dimB+dimS, 32]
 const layers_num = length(layer) - 1
 const networkdim = layer[2] * (layer[1] + 1) + layer[1]
 
 # Learning Rate
 const lr = 1f-3
-const dt = 1f-5
+const dt = 1f-3
 const batchsize = 64
 
 end
